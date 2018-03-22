b768503948e9f0641597d4b6214971aac7bb5fce
Max Levin <msl@lclark.edu>
2018-03-22 16:53:00 -0700

    Linear regression with sklearn for single case Refactored TODO's Cleaned up some code

--- Polynomial Regression/polynomial_regression.py
+++ Polynomial Regression/polynomial_regression.py
@@ -7,57 +7,116 @@ Max, Will, Peter, Ben
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 
-# ======== Generate Data ======== #
-
-def build_function(degree):
-	""" Builds a polynomial of given degree with coefficients uniformly distributed between -0.5 and 0.5. """
-	coeffs = np.random.random(degree + 1) - 0.5
+def build_function(degree, coeffs=np.zeros((1, 1))):
+	""" Builds a polynomial of given degree with random coefficients. """
+	if not coeffs.any():
+		coeffs = np.random.random(degree + 1) - 0.5
 	return lambda x: sum([a * x ** i for i, a in enumerate(coeffs)])
 
 
 def steve(numPoints):
 	""" Generates random x values between -2 and 2. """
-	return 4 * np.random.random(numPoints) - 2
+	return 4 * np.random.random((numPoints, 1)) - 2
 
 
-# Add random noise (normally distributed with mean 0 and standard deviation 1) to each y value
 def getYValsWithNoise(xVals, func):
 	""" Generate y values given a set of x values and a function. Adds noise that is normally distributed with mean 0 and standard deviation 1. """
 	return np.array([func(x) + np.random.normal() for x in xVals])
 
 
-# Generate testing data without noise, using 100 'x' values uniformly distributed on the interval [-2, 2]
 def getYVals(xVals, func):
 	""" Generate y values given a set of x values and a function. """
 	return np.array([func(x) for x in xVals])
 
 
-# TODO: For testing purposes (not final product), plot training and testing data
+def meanSquareError(func_a, func_b, x_vals):
+	""" returns the mean squared error of two functions with a shared np.array of x values """
+	summation = 0
+	for x in x_vals:
+		summation += (func_a(x) - func_b(x)) ** 2
+	return summation / x_vals.size
 
-test_data_1 = 0
 
-f2 = build_function(2)
+# ======== Generate Testing Data ======== #
+
+# Random Polynomial functions used to generate data
+f1, f2, f20 = build_function(1), build_function(2), build_function(20)
+
+# Test data for 10 points
+x_test_10 = steve(10)
+y_test_d1_10, y_test_d2_10, y_test_d20_10 = getYVals(x_test_10, f1), getYVals(x_test_10, f2), getYVals(x_test_10, f20)
+
+# Test data for 100 points
+x_test_100 = steve(100)
+y_test_d1_100, y_test_d2_100, y_test_d20_100 = getYVals(x_test_100, f1), getYVals(x_test_100, f2), getYVals(x_test_100,
+                                                                                                            f20)
+
+# Test data for 1000 points
+x_test_1000 = steve(1000)
+y_test_d1_1000, y_test_d2_1000, y_test_d20_1000 = getYVals(x_test_1000, f1), getYVals(x_test_1000, f2), getYVals(
+		x_test_1000, f20)
+
+# ======== Generate Training Data ======== #
+
+# Training data for 10 points
+x_train_10 = steve(10)
+y_train_d1_10, y_train_d2_10, y_train_d20_10 = getYValsWithNoise(x_train_10, f1), getYValsWithNoise(x_train_10,
+                                                                                                    f2), getYValsWithNoise(
+		x_train_10, f20)
+
+# Training data for 100 points
+x_train_100 = steve(100)
+y_train_d1_100, y_train_d2_100, y_train_d20_100 = getYValsWithNoise(x_train_100, f1), getYValsWithNoise(x_train_100,
+                                                                                                        f2), getYValsWithNoise(
+		x_train_100,
+		f20)
+
+# Training data for 1000 points
+x_train_1000 = steve(1000)
+y_train_d1_1000, y_train_d2_1000, y_train_d20_1000 = getYValsWithNoise(x_train_1000, f1), getYValsWithNoise(
+		x_train_1000, f2), getYValsWithNoise(
+		x_train_1000, f20)
+
+# ======== Plot Stuff ======== #
+f2 = build_function(3)
 x1 = steve(100)
-y1 = getYVals(x1, f2)
+y1 = getYValsWithNoise(x1, f2)
 
+poly_features = PolynomialFeatures(degree=3, include_bias=False)
+X_poly = poly_features.fit_transform(x1)
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y1)
+
+coefficients = np.insert(lin_reg.coef_, 0, lin_reg.intercept_)
+
+x_values = np.arange(-2, 2, .001)
+guess_y = getYVals(x_values, build_function(2, coefficients))
+real_y = getYVals(x_values, f2)
+
+plt.plot(x_values, real_y, color="green", linewidth=3)
+plt.plot(x_values, guess_y, color="orange", linewidth=3)
 plt.scatter(x1, y1)
 
 plt.xlim((-2, 2))
+# plt.ylim((-10,10))
 plt.show()
 
-# ======== Fit Polynomials ======== #
+print(meanSquareError(f2, build_function(2, coefficients), x_values))
 
-# TODO: Use sklearn.linear_model.LinearRegression to fit polynomials of various degrees to the training data
+# ======== PROJECT REQUIREMENTS ======== #
 
-# TODO: Be able to produce plots showing the training and testing data, true function, and several approximation functions
+# TODO: A single run showing 10 training points, true test curve, and fit polynomials of 1, 2, and 20 degrees
 
-# TODO: Do this for 10, 100, and 1000 data points for polynomials of degrees 1, 2, and 20
+# TODO: Ditto but for 100 training points
 
+# TODO: Ditto but for 1000 training points
 
-# ======== Compare Polynomials ======== #
+# TODO: Training and testing mean squared error, averaged over 100 runs with 10 training points each, as a function of model degree 0 through 20
 
-# TODO: Repeat the above experiment 100 times, using all of the model polynomial degrees 0 through 20.
+# TODO: Ditto but for 100 training points
 
-# TODO: For 10, 100, and 1000 data points, produce a plot showing the average mean squared error (across all 100 runs) for the training and testing sets
+# TODO: Ditto but for 1000 training points
