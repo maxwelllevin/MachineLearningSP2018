58f87fabb3b9c1a79eeecd820453a7b7a9f714df
Maxwell <maxwellevin@users.noreply.github.com>
2018-04-22 14:10:34 -0700

    Initial Commit

---
+++ Deep Learning/deep_learning.py
@@ -0,0 +1,10 @@
+""""
+CS 369: AI & Machine Learning
+Lab #3: Deep Learning
+April 26, 2018
+
+In this project we build a powerful deep learning system for image recognition for tiny images in the CIFAR-10 data set.
+https://www.cs.toronto.edu/~kriz/cifar.html
+
+Max, Will, Peter, Ben
+"""
